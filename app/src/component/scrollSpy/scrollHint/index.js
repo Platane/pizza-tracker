a2d3af18c697945795a661962f8acc4f0c30643a
Platane <me@platane.me>
2017-04-10 13:20:27 +0200

    :rocket: better scroll hint

--- app/src/component/scrollSpy/scrollHint/index.js
+++ app/src/component/scrollSpy/scrollHint/index.js
@@ -13,10 +13,13 @@ export const ScrollHint = ({ displayed }) =>
             ({ next, transition }) =>
                 next || transition
                     ? (
-                        <svg viewBox="-80 -80 160 160" className={c(style.container, transition && style.transition, next && style.visible, !next && style.notVisible) } title="scroll">
-                            <circle className={style.shadow} cx={0} cy={0} r={80}/>
-                            <circle className={style.circle} cx={0} cy={0} r={45}/>
-                            <path className={style.arrow} d="M-25 -2 L0 18 L25 -2"/>
+                        <svg viewBox="-120 -120 240 240" className={c(style.container, transition && style.transition, next && style.visible, !next && style.notVisible) } title="scroll">
+                            <path className={style.shadow} d="M-100 -20 Q-100 -120 0 -120 Q100 -120 100 -20 L100 20 Q100 120 0 120 Q-100 120 -100 20z"/>
+                            <g className={style.mouse} transform="translate(0,-24)">
+                                <path className={style.tic} d="M0 -2 L0 2"/>
+                                <path className={style.potato} d="M-25 -20 Q-25 -45 0 -45 Q25 -45 25 -20 L25 20 Q25 45 0 45 Q-25 45 -25 20z"/>
+                                <text className={style.text} x={0} y={90} >scroll</text>
+                            </g>
                         </svg>
                     )
                     : null
