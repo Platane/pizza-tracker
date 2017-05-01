9a0e8d71ee51d36dd768b76e2d0eb53a0dbbd202
Platane <me@platane.me>
2017-05-01 13:31:07 +0200

    :hammer: prevent shader optimisation ( which made the app crash in some devices )

--- app/src/component/chart/renderer/object/line/fragment_fs.glsl
+++ app/src/component/chart/renderer/object/line/fragment_fs.glsl
@@ -12,7 +12,18 @@ void main(void) {
 
     float dash = mod( vLength*2.0, 1.0 );
 
-    if ( uk < vX || ( vDash > 0.0 && dash < 0.4 ) ){
+    if (
+        // do not display if the point is after k
+        uk < vX
+        ||
+
+        // do not display if the point is in a empty dash segment
+        ( vDash > 0.0 && dash < 0.4 )
+        ||
+
+        // dummy use of vNormal, to prevent it from being removed at optimisation ( and therefor crash the app when trying to bind to a removed attribute )
+        vNormal.x > 99.9
+        ){
         discard;
     }
 
