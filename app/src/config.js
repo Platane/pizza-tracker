669af3979f45adf8497d68805ec9a57a52bf2d27
Platane <me@platane.me>
2017-07-19 12:58:49 +0200

    :rocket: add new player

--- app/src/config.js
+++ app/src/config.js
@@ -1,6 +1,10 @@
 export const users = [
     {
+        userName :  'u',
+        data_url :  'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw/insaneJMad',
+    },
+    {
         userName :  'x',
-        data_url :  'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw',
+        data_url :  'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw/mrhelmut%2520',
     }
 ]
