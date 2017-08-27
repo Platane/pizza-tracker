262ba5a0f7de69b311ee3b8757afa08e8096673e
platane <me@platane.me>
2017-08-27 22:45:13 +0200

    :rocket: fix typo preventing from loading the latest revision

--- app/src/config.js
+++ app/src/config.js
@@ -1,10 +1,12 @@
 export const users = [
     {
-        userName :  'u',
-        data_url :  'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw/insaneJMad',
+        userName: 'u',
+        data_url:
+            'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw/insaneJMad',
     },
     {
-        userName :  'x',
-        data_url :  'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw/mrhelmut%2520',
-    }
+        userName: 'x',
+        data_url:
+            'https://gist.githubusercontent.com/Platane/20026468d92c5d63a6fe71265d1fda08/raw/mrhelmut',
+    },
 ]
