9630d32d576e746e7769f98ae9edb8504684ca4e
Platane <me@platane.me>
2021-06-16 20:46:21 +0200

    Update yearTitles.ts

--- app/assets/content/yearTitles.ts
+++ app/assets/content/yearTitles.ts
@@ -2,5 +2,6 @@ export default {
   "2017": "The primal pizza expansion",
   "2018": "The great recession",
   "2019": "The cursed oauth",
+  "2020": "A pretty sad year",
   default: "One more year"
 };
